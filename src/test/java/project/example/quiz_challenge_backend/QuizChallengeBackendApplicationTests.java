373b903fa267b6181088fe97a7ec8393cfd0c972
0320kangk <0320kangk@naver.com>
2024-05-07 16:19:12 +0900

    add readme file

---
+++ src/test/java/project/example/quiz_challenge_backend/QuizChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package project.example.quiz_challenge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizChallengeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
